f7082b41ee4064b3ef42fa89437ffda767177d9d
igor <igor.bezverkhyi@globallogic.com>
2019-09-18 18:50:49 +0300

    Validation added

--- profitool/src/main/java/com/ihbez/profitool/web/ProjectController.java
+++ profitool/src/main/java/com/ihbez/profitool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.ihbez.profitool.web;
 
 import com.ihbez.profitool.domain.Project;
+import com.ihbez.profitool.services.MapValidationErrorService;
 import com.ihbez.profitool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,20 +25,13 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        if (result.hasErrors()) {
-
-            Map<String,String> errorMap = new HashMap<>();
-
-            for (FieldError error: result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
-        }
-
-
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap!=null) return errorMap;
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project,HttpStatus.CREATED);
     };
